7c16185cdcddde931eb63fbe5fc4516d069c0a1a
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-16 23:39:43 +0100

    added home to settings.py

--- autoskunkworks/settings.py
+++ autoskunkworks/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_forms_bootstrap,'
+    'django_forms_bootstrap',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'autoskunkworks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
