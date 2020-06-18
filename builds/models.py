fcb821ca939fd975ad70dc023c17ffa6aa6971f3
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-18 21:17:05 +0100

    added category models

--- builds/models.py
+++ builds/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class ExteriorCategory(models.Model):
+    """Exterior Categories"""
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class EngineCategory(models.Model):
+    """Engine Categories"""
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class RunningCategory(models.Model):
+    """Running Gear Categories"""
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class InteriorCategory(models.Model):
+    """Interior Categories"""
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
