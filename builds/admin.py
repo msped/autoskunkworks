fbbd1066c9f96da78dc41c4f6cbd4f8627d924c1
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-18 21:18:59 +0100

    added categories to admin

--- builds/admin.py
+++ builds/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ExteriorCategory, EngineCategory, InteriorCategory, RunningCategory
 
 # Register your models here.
+
+admin.site.regsiter(ExteriorCategory)
+admin.site.regsiter(EngineCategory)
+admin.site.regsiter(InteriorCategory)
+admin.site.regsiter(RunningCategory)
