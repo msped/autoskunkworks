006cbe988c016486bddd69bedc9a5f0f06e79957
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-20 19:48:37 +0100

    added create builld view

---
+++ builds/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from .models import (
+    ExteriorCategory,
+    EngineCategory,
+    RunningCategory,
+    InteriorCategory
+)
+from .utils import new_build_content
+
+# Create your views here.
+
+def create_build(request):
+    """Create a Build"""
+    exterior_category = ExteriorCategory.objects.all()
+    engine_category = EngineCategory.objects.all()
+    running_category = RunningCategory.objects.all()
+    interior_category = InteriorCategory.objects.all()
+
+    if request.method == "POST":
+        new_build_content(
+            request,
+            exterior_category,
+            engine_category,
+            running_category,
+            interior_category
+        )
+        return redirect('home')
+
+    context = {
+        'exterior': exterior_category,
+        'engine': engine_category,
+        'running': running_category,
+        'interior': interior_category
+    }
+    
+    return render(request, "create.html", context)
