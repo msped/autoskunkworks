56d28c2c99e0723ac09cde1d191200ed6028ac51
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-21 15:02:32 +0100

    added view url

--- builds/urls.py
+++ builds/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import create_build
+from .views import create_build, view_build
 
 urlpatterns = [
     url(r'^create', create_build, name="create_build"),
+    url(r'^(?P<build_id>\d+)', view_build, name="view_build"),
 ]
