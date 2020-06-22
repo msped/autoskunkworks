3cca59f72736931da9fc0834deca65fdbcf11a8d
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-22 20:48:54 +0100

    added dislike url

--- builds/urls.py
+++ builds/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import create_build, view_build, builds, like_build
+from .views import create_build, view_build, builds, like_build, dislike_build
 
 urlpatterns = [
     url(r'^create', create_build, name="create_build"),
     url(r'^(?P<build_id>\d+)', view_build, name="view_build"),
     url(r'^$', builds, name="builds"),
     url(r'^like/(?P<build_id>\d+)', like_build, name="like_build"),
+    url(r'^dislike/(?P<build_id>\d+)', dislike_build, name="dislike_build"),
 ]
