34a3e4dede52a11c60d643e22a27755f592ca2c6
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-22 20:47:08 +0100

    added like url

--- builds/urls.py
+++ builds/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import create_build, view_build, builds
+from .views import create_build, view_build, builds, like_build
 
 urlpatterns = [
     url(r'^create', create_build, name="create_build"),
     url(r'^(?P<build_id>\d+)', view_build, name="view_build"),
     url(r'^$', builds, name="builds"),
+    url(r'^like/(?P<build_id>\d+)', like_build, name="like_build"),
 ]
