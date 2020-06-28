ffd807103648e6e48d2127fbedb5688712e48575
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-06-28 20:47:37 +0100

    added edit and delete_row urls

--- builds/urls.py
+++ builds/urls.py
@@ -1,5 +1,14 @@
 from django.conf.urls import url
-from .views import create_build, view_build, builds, like_build, dislike_build, users_builds
+from .views import (
+    create_build,
+    view_build,
+    builds,
+    like_build, 
+    dislike_build,
+    users_builds,
+    edit_build,
+    delete_row,
+)
 
 urlpatterns = [
     url(r'^create', create_build, name="create_build"),
@@ -7,5 +16,7 @@ urlpatterns = [
     url(r'^$', builds, name="builds"),
     url(r'^like/(?P<build_id>\d+)', like_build, name="like_build"),
     url(r'^dislike/(?P<build_id>\d+)', dislike_build, name="dislike_build"),
-    url(r'^(?P<username>[\w.@+-]+)', users_builds, name="users_builds")
+    url(r'^/u/(?P<username>[\w.@+-]+)', users_builds, name="users_builds"),
+    url(r'^edit/(?P<build_id>\d+)', edit_build, name="edit_build"),
+    url(r'^delete_row/(?P<row_id>\d+)/(?P<table>[\w-]+)', delete_row, name="delete_row"),
 ]
