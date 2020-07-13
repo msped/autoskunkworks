4f6c7ff2fbd976131d05aabf863ca7ef9c8159e9
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-07-13 21:31:35 +0100

    changed email to username

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,12 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserLoginForm(forms.Form):
     """Form to log a user in"""
-    username = forms.CharField(label="E-mail")
+    username = forms.CharField(label="Username")
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['E-mail', 'Password']
+        fields = ['Username', 'Password']
 
 class UserRegisterForm(UserCreationForm):
     """Form for registering a new user"""
