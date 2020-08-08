6eebaa4a567fb3e2d7ae83cd31e70c6cbedc7102
Matt Edwards <38842422+msped@users.noreply.github.com>
2020-08-08 18:32:15 +0100

    add support qpp tests

--- support/tests.py
+++ support/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .apps import SupportConfig
 
 # Create your tests here.
+
+class TestSupportApp(TestCase):
+    def test_support_page_response(self):
+        """Test page responpse"""
+        response = self.client.get(
+            '/s/'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>Support</h1>', response.content)
+
+    def test_support_app(self):
+        """Test Support App"""
+        self.assertEqual("support", SupportConfig.name)
