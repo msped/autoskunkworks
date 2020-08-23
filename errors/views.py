48cdd68024541e71ed45a4acc829ee96e78144b2
Matt <mattsp.edwards@googlemail.com>
2020-08-23 16:06:04 +0100

    added 404 and 500 views

--- errors/views.py
+++ errors/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from sentry_sdk import last_event_id
 
 # Create your views here.
+
+def handler404(request, exception):
+    return render(request, "404.html", status=404)
+
+def handler500(request, *args, **argv):
+    return render(request, "500.html", {
+        'sentry_event_id': last_event_id(),
+    }, status=500)
