0ab57461b3be112321f73d1d6e847ffa7c1aacd0
Matt <mattsp.edwards@googlemail.com>
2020-08-24 22:35:44 +0100

    added NewTicket form

---
+++ errors/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Issue
+
+class NewTicket(forms.ModelForm):
+    """Form for new ticket from model"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs['placeholder'] = 'Please describe in much detail as possible your actions leading to this issue or the proposed enhancement.'
+
+    class Meta: 
+        model = Issue
+        fields = [
+            'issue_location',
+            'description'
+        ]
