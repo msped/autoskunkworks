2915f9439460dd3725a09232e701ce6432dc9dbd
Matt <mattsp.edwards@googlemail.com>
2020-08-25 22:12:53 +0100

    added tests for new contact form

--- support/tests.py
+++ support/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .forms import ContactForm
 from .apps import SupportConfig
 
 # Create your tests here.
@@ -15,3 +16,80 @@ class TestSupportApp(TestCase):
     def test_support_app(self):
         """Test Support App"""
         self.assertEqual("support", SupportConfig.name)
+    
+    def test_contact_post(self):
+        """Test post for contact page"""
+        response = self.client.post(
+            '/s/',
+            data={
+                'email': "test@google.com",
+                'subject': "Test Subject",
+                'message': "Here is a test message"
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_form_in_post(self):
+        """Test invalid for data and return variables"""
+        response = self.client.post(
+            '/s/',
+            {
+                'email': "",
+                'subject': "Test Subject",
+                'message': "Here is a test message"
+            }
+        )
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'sent': False,
+                'error': 'Invalid Form'
+            }
+        )
+
+class TestContactForm(TestCase):
+    """Contact form tests"""
+    def test_contact_form_valid_response(self):
+        """Test full working contact form"""
+        form = ContactForm({
+            'email': 'test@email.com',
+            'subject': 'test subject',
+            'message': 'test message'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_contact_form_invalid_email(self):
+        """Test full working contact form"""
+        form = ContactForm({
+            'email': 'testemail.com',
+            'subject': 'test subject',
+            'message': 'test message'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_contact_form_invalid_subject(self):
+        """Test full working contact form"""
+        form = ContactForm({
+            'email': 'test@email.com',
+            'subject': '',
+            'message': 'test message'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_contact_form_invalid_message(self):
+        """Test full working contact form"""
+        form = ContactForm({
+            'email': 'test@email.com',
+            'subject': 'test subject',
+            'message': ''
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_contact_form_empty(self):
+        """Test empty contact form"""
+        form = ContactForm({
+            'email': '',
+            'subject': '',
+            'message': ''
+        })
+        self.assertFalse(form.is_valid())
