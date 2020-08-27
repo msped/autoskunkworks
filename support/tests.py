9e1c2b7bee7b7bbf390c553dce0f37711c069d61
Matt <mattsp.edwards@googlemail.com>
2020-08-28 00:27:36 +0100

    add tes for contactform initial

--- support/tests.py
+++ support/tests.py
@@ -1,10 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .forms import ContactForm
 from .apps import SupportConfig
 
 # Create your tests here.
 
 class TestSupportApp(TestCase):
+    def setUp(self):
+        self.user = {
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'username': 'mspe',
+            'email': 'test@gmail.com',
+            'password': 'testpassword'
+        }
+        User.objects.create_user(**self.user)
+
     def test_support_page_response(self):
         """Test page responpse"""
         response = self.client.get(
@@ -28,6 +39,17 @@ class TestSupportApp(TestCase):
             }
         )
         self.assertEqual(response.status_code, 200)
+
+    def test_initail_in_contact_form(self):
+        """Test that when a user is logged in their email
+        is displayed automatically in the contact form"""
+        self.client.post(
+            '/u/login/',
+            self.user,
+            follow=True
+        )
+        response = self.client.get('/s/')
+        self.assertIn(b'<input type="email" name="email" value="test@gmail.com" class=" form-control" required id="id_email">', response.content)
 
     def test_invalid_form_in_post(self):
         """Test invalid for data and return variables"""
