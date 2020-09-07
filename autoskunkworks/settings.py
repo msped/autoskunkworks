dc68294a5740fe1cf95059d21b3daf86b4851b09
Matt <mattsp.edwards@googlemail.com>
2020-09-07 17:32:13 +0100

    added localhost ip to allowed hosts

--- autoskunkworks/settings.py
+++ autoskunkworks/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'autoskunk.works', 'www.autoskunk.works', '165.232.107.91']
+ALLOWED_HOSTS = ['localhost', 'autoskunk.works', 'www.autoskunk.works', '165.232.107.91', '127.0.0.1']
 
 
 # Application definition
