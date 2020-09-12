0c5954e79023d2f83a60c37ca175812d6695d6ac
Matt <mattsp.edwards@googlemail.com>
2020-09-12 17:42:17 +0100

    added passwordresetview for customization

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect, reverse
+from django.views.generic.edit import FormView
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse_lazy
 from django.contrib import auth, messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.core.paginator import Paginator
 from builds.utils import sort_builds_users, sort_builds_users_public
 from builds.models import Builds
 from .utils import signup_email, deactivate_email
-from .forms import UserLoginForm, UserRegisterForm, Profile
+from .forms import UserLoginForm, UserRegisterForm, Profile, PasswordResetForm
 
 def login(request):
     """Logs a user in / display login page"""
@@ -154,3 +160,47 @@ def users_builds(request, username):
 
     return render(request, "my_builds.html", {"builds": builds_paginator,
                                               'user': user})
+
+class PasswordContextMixin:
+    extra_context = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': self.title,
+            **(self.extra_context or {})
+        })
+        return context
+
+class PasswordResetView(PasswordContextMixin, FormView):
+    email_template_name = 'registration/password_reset_email.html'
+    extra_email_context = None
+    form_class = PasswordResetForm
+    from_email = None
+    html_email_template_name = None
+    subject_template_name = 'registration/password_reset_subject.txt'
+    success_url = reverse_lazy('password_reset_done')
+    template_name = 'registration/password_reset_form.html'
+    title = _('Password reset')
+    token_generator = default_token_generator
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        opts = {
+            'use_https': self.request.is_secure(),
+            'token_generator': self.token_generator,
+            'from_email': self.from_email,
+            'email_template_name': self.email_template_name,
+            'subject_template_name': self.subject_template_name,
+            'request': self.request,
+            'html_email_template_name': self.html_email_template_name,
+            'extra_email_context': self.extra_email_context,
+        }
+        form.save(**opts)
+        return super().form_valid(form)
+
+
+INTERNAL_RESET_SESSION_TOKEN = '_password_reset_token'
