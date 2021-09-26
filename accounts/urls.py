417305cc3bbf3d074a407539aa350138a12d3d52
Matt Edwards <mattsp.edwards@googlemail.com>
2021-09-26 16:31:30 +0100

    changed account urls to use path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url, include
-from .views import login, logout, register, change_password, users_builds, settings, update_profile, delete_account
+from django.urls import path, include
+from .views import Login, logout, Register, change_password, users_builds, settings, update_profile, delete_account
 from accounts import urls_reset
 
 urlpatterns = [
-    url(r'^login/', login, name="login"),
-    url(r'^register/', register, name="register"),
-    url(r'^logout/', logout, name="logout"),
-    url(r'^password-reset/', include(urls_reset)),
-    url(r'^change-password/', change_password, name="change_password"),
-    url(r'^update-profile/', update_profile, name="update_profile"),
-    url(r'^delete-account/', delete_account, name="delete_account"),
-    url(r'^settings/', settings, name="settings"),
-    url(r'^(?P<username>[\w.@+-]+)', users_builds, name="users_builds"),
+    path('login/', Login.as_view(), name="login"),
+    path('register/', Register.as_view(), name="register"),
+    path('logout/', logout, name="logout"),
+    path('password-reset/', include(urls_reset)),
+    path('change-password/', change_password, name="change_password"),
+    path('update-profile/', update_profile, name="update_profile"),
+    path('delete-account/', delete_account, name="delete_account"),
+    path('settings/', settings, name="settings"),
+    path('<str:username>', users_builds, name="users_builds"),
 ]
