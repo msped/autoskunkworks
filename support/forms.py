a4c94f9527516852d75049982dd4bf45a779f1ee
Matt Edwards <mattsp.edwards@googlemail.com>
2021-09-26 16:38:04 +0100

    added captcha field to contactform

--- support/forms.py
+++ support/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from captcha.fields import CaptchaField
 
 class ContactForm(forms.Form):
     """Form to be displayed on contact page"""
     email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
+    captcha = CaptchaField()
